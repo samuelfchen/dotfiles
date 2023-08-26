d8306cf66d5a522b37662927c5bc4b1c5f1d2fd1
Samuel Chen <samuel.chen@akunacapital.com>
2023-08-26 17:01:40 +1000

    add yabai configs

--- .config/nvim/lua/samuel/plugins/lsp/mason.lua
+++ .config/nvim/lua/samuel/plugins/lsp/mason.lua
@@ -28,7 +28,7 @@ mason_lspconfig.setup({
     "tailwindcss",
     "lua_ls",
     "emmet_language_server",
-    "json-lsp"
+    "json_ls"
   },
   -- auto-install configured servers (with lspconfig)
   automatic_installation = true, -- not the same as ensure_installed
