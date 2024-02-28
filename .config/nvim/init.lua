022dae1a20ba6e51c124de7f8b209adbd0facf01
Samuel Chen <samuel.chen@airwallex.com>
2024-02-28 21:14:07 +1100

    split up kickstart config into multiple files

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,2 +1,4 @@
-require("kickstart-init")
 require("user")
+
+-- The line beneath this is called `modeline`. See `:help modeline`
+-- vim: ts=2 sts=2 sw=2 et
--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -3,7 +3,14 @@ return {
     'stevearc/oil.nvim',
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
-      require('oil').setup({})
+      require('oil').setup({
+        keymaps = {
+          ["q"] = "actions.close",
+          ["<C-h>"] = false,
+          ["<C-s>"] = false,
+          ["<C-l>"] = false
+        }
+      })
 
       vim.keymap.set('n', "-", "<cmd>Oil<cr>")
     end
