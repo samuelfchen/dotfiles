02557d3be84f290a818dc0326aece0705a94c77a
Samuel Chen <samuel.chen@airwallex.com>
2024-02-28 21:48:00 +1100

    maximizer

---
+++ .config/nvim/lua/plugins/maximizer.lua
@@ -0,0 +1,8 @@
+return {
+  {
+    "szw/vim-maximizer",
+    config = function()
+      vim.keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>") -- toggle split window maximization
+    end
+  },
+}
