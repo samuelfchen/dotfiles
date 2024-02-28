9f8373853d70efa1a74cebf1c2c2aef5169a30a7
Samuel Chen <samuel.chen@airwallex.com>
2024-02-28 22:14:47 +1100

    webify

--- .config/nvim/lua/plugins/webify_git.lua
+++ .config/nvim/lua/plugins/webify_git.lua
@@ -1,3 +1,7 @@
 return {
-  "pabloariasal/webify.nvim"
+  "pabloariasal/webify.nvim",
+  config = function()
+    vim.keymap.set("n", "<leader>gyl", "<cmd>YankLineUrl +<cr>")
+    vim.keymap.set("n", "<leader>gyf", "<cmd>YankFileUrl +<cr>")
+  end
 }
