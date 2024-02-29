7a39cb21b8483ab7a0295b7d3696a4e3a22b3ded
Samuel Chen <samuel.chen@airwallex.com>
2024-02-29 13:08:41 +1100

    symbols outline

--- .config/nvim/lua/user/options.lua
+++ .config/nvim/lua/user/options.lua
@@ -7,6 +7,7 @@ vim.o.hlsearch = true
 
 -- Make line numbers default
 vim.wo.number = true
+vim.wo.relativenumber = true
 
 -- Enable mouse mode
 vim.o.mouse = 'a'
@@ -45,4 +46,3 @@ vim.o.cursorline = true
 -- split windows
 vim.o.splitright = true
 vim.o.splitbelow = true
-
