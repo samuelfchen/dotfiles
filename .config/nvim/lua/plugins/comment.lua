4d140dba638a26d8391926c22c7b07793d251323
Samuel Chen <samuel.chen@airwallex.com>
2024-02-29 12:26:50 +1100

    comment keymaps

--- .config/nvim/lua/plugins/comment.lua
+++ .config/nvim/lua/plugins/comment.lua
@@ -1,4 +1,15 @@
 return {
   -- "gc" to comment visual regions/lines
-  { 'numToStr/Comment.nvim', opts = {} },
+  {
+    'numToStr/Comment.nvim',
+    dependencies = {
+      'JoosepAlviste/nvim-ts-context-commentstring',
+    },
+    opts = {
+      toggler = { line = '<leader>cc', block = '<leader>bc' },
+      opleader = { line = '<leader>c', block = '<leader>b' },
+      extra = { above = '<leader>cO', below = '<leader>co', eol = '<leader>cA' },
+      pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
+    },
+  },
 }
